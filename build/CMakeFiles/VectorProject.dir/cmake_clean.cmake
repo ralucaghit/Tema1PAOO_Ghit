729e984a43c7ef5dcf35071d6c5da30863507d63
deliailia <delia.ilia@yahoo.com>
2024-11-04 00:42:46 +0200

    Cerinta cu cmake

---
+++ build/CMakeFiles/VectorProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VectorProject.dir/tema.cpp.o"
+  "VectorProject"
+  "VectorProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VectorProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
