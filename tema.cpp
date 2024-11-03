5ebf292bbcd0d23a38a970ae5e17695e5481cef8
deliailia <delia.ilia@yahoo.com>
2024-11-04 00:10:26 +0200

    Cerintele : 2,3,4 si copy constructor

--- tema.cpp
+++ tema.cpp
@@ -23,6 +23,18 @@ public:
         }
     }
 
+    // Copy constructor
+    Vector(const Vector& other) : size(other.size) {
+        elements = new int[size];
+        for (int i = 0; i < size; ++i) {
+            elements[i] = other.elements[i];
+        }
+        cout << "Copy constructor: " << endl;  
+    } 
+
+   
+
+
     //destructor pentru a elibera memoria
     ~Vector() {
         delete[] elements;
@@ -62,5 +74,17 @@ int main() {
     cout << "Suma vectorilor: ";
     sum.print();
 
+
+    // Folosim constructorul de copiere
+    Vector v3(v1);  
+    cout << "Vectorul copiat (v3): ";
+    v3.print();
+
+    // Afișăm valorile înainte de modificare
+    cout << "Vector original (v1): ";
+    v1.print();
+   
+    
+    
     return 0;
 }
